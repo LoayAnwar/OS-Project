63eb44edc84f267f9e138833726c78c89be65f08
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-04-25 18:10:29 +0200

    Part of gui

--- OS-Project/mainwindow.cpp
+++ OS-Project/mainwindow.cpp
@@ -1,8 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
-//#include <QTableWidget>
-//#include <QTableView>
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -18,6 +17,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->tableWidget->hide();
     ui->lineEdit_2->hide();
     ui->label_2->hide();
+    ui->submit->hide();
 
 }
 
@@ -29,27 +29,65 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->lineEdit_2->hide();
-    ui->label_2->hide();
-    ui->tableWidget->show();
-    ui->tableWidget->setColumnCount(2);
-    QString string = ui->lineEdit->text();
-    ui->tableWidget->setRowCount(string.toInt());
-    QStringList labels;
-    labels << "Arrival Time" << "Burst Time";
-    QStringList labels_2;
-    for (int i = 0; i < string.toInt(); i++) {
-        labels_2 << "P" + QString::number(i);
+    if(ui->comboBox->currentIndex()==-1 || (ui->lineEdit->text() == "")){
+        QMessageBox::warning(this, "Wrong Input", "Please choose process or enter value");
+        ui->tableWidget->hide();
+        ui->submit->hide();
     }
-    ui->tableWidget->setVerticalHeaderLabels(labels_2);
+    else {
+        ui->lineEdit_2->hide();
+        ui->label_2->hide();
+        ui->tableWidget->show();
+        ui->submit->show();
+        ui->tableWidget->setColumnCount(2);
+        QString string = ui->lineEdit->text();
+        ui->tableWidget->setRowCount(string.toInt());
+        QStringList labels;
+        labels << "Arrival Time" << "Burst Time";
+        QStringList labels_2;
+        for (int i = 0; i < string.toInt(); i++) {
+            labels_2 << "P" + QString::number(i);
+        }
+        ui->tableWidget->setVerticalHeaderLabels(labels_2);
 
-    if (ui->comboBox->currentIndex() == 3 || ui->comboBox->currentIndex() == 4){
-        labels << "Priority";
-        ui->tableWidget->insertColumn(1);
+        if (ui->comboBox->currentIndex() == 3 || ui->comboBox->currentIndex() == 4){
+            labels << "Priority";
+            ui->tableWidget->insertColumn(1);
+        }
+        else if (ui->comboBox->currentIndex() == 5) {
+            ui->lineEdit_2->show();
+            ui->label_2->show();
+        }
+        ui->tableWidget->setHorizontalHeaderLabels(labels);
     }
-    else if (ui->comboBox->currentIndex() == 5) {
-        ui->lineEdit_2->show();
-        ui->label_2->show();
-    }
-    ui->tableWidget->setHorizontalHeaderLabels(labels);
 }
+
+void MainWindow::on_submit_clicked()
+{
+    QString process =  ui->comboBox->currentText();
+    if(process == "FCFS")
+    {
+
+    }
+    else if (process == "SJF (Preemptive)")
+    {
+
+    }
+    else if (process == "SJF (Non Preemptive)")
+    {
+
+    }
+    else if (process == "Priority (Non Preemptive)")
+    {
+
+    }
+    else if (process == "Priority (Preemptive)")
+    {
+
+    }
+    else if(process == "Round Robin")
+    {
+
+    }
+}
+
